9ea171e894c2088cc641eec45c72d9c909d91acb
Cedric Haegele <zikced@yahoo.fr>
2024-02-02 11:23:13 +0100

    maj slidingPanelayout

--- app/src/main/java/com/example/realestatemanager/MainActivity.java
+++ app/src/main/java/com/example/realestatemanager/MainActivity.java
@@ -2,21 +2,26 @@ package com.example.realestatemanager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.slidingpanelayout.widget.SlidingPaneLayout;
+
 import com.example.realestatemanager.LoanSimulator.LoanSimulatorFragment;
 import com.example.realestatemanager.Map.MapFragment;
 import com.example.realestatemanager.RealtyAdd.RealtyAdd;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    SlidingPaneLayout pane;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.activity_main_toolbar);
         setSupportActionBar(toolbar);
 
-        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white));
 
         DrawerLayout drawerLayout = findViewById(R.id.activity_main_drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -34,20 +39,20 @@ public class MainActivity extends AppCompatActivity {
         toggle.syncState();
 
         NavigationView navigationView = findViewById(R.id.activity_main_nav_view);
+
         navigationView.setNavigationItemSelectedListener(item -> {
             int id = item.getItemId();
 
             if (id == R.id.map_drawer) {
-                replaceFragment(new MapFragment());
-
+                replaceFragment(new MapFragment(), true);
             } else if (id == R.id.action_loan_simulator) {
-                replaceFragment(new LoanSimulatorFragment());
+                replaceFragment(new LoanSimulatorFragment(), false);
             }
 
-            DrawerLayout drawerLayout1 = findViewById(R.id.activity_main_drawer_layout);
-            drawerLayout1.closeDrawer(GravityCompat.START);
+            drawerLayout.closeDrawer(GravityCompat.START);
             return true;
         });
+
     }
 
     @Override
@@ -70,9 +75,16 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void replaceFragment(Fragment fragment) {
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.map_container, fragment)
-                .commit();
+    private void replaceFragment(Fragment fragment, boolean isMaster) {
+        int containerId = isMaster ? R.id.left_pane : R.id.right_pane;
+        if (findViewById(containerId) != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(containerId, fragment)
+                    .commit();
+        } else {
+            Log.e("MainActivity", "Conteneur pour le fragment introuvable.");
+        }
     }
+
+
 }
